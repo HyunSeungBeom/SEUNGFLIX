044dd72f5f064eec58eec390fd742206a8fde317
seungbeom <ghfk100@naver.com>
2022-10-30 21:46:49 +0900

    feat: 홈화면 배너 이미지 추가

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { IGetMoviesResult } from "../types/type";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -8,6 +9,8 @@ const baseApi = axios.create({
 });
 
 export const getMoives = async () => {
-  const res = await baseApi.get(`/movie/now_playing?api_key=${API_KEY}`);
+  const res = await baseApi.get<IGetMoviesResult>(
+    `/movie/now_playing?api_key=${API_KEY}`
+  );
   return res;
 };
